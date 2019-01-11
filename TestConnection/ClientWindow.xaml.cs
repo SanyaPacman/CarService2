c356e09d9c39500c2d3ad9637a75371d1ddac0f5
Olga Timofeeva <38052124+olgatimofeeva@users.noreply.github.com>
2019-01-11 22:32:37 +0300

    Front

---
+++ TestConnection/ClientWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TestConnection
+{
+    /// <summary>
+    /// Логика взаимодействия для ClientWindow.xaml
+    /// </summary>
+    public partial class ClientWindow : Window
+    {
+        public ClientWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        public void Button_MainWindow(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        public void Add_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+          
+        public void Edit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void Delete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void Save_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
